cef74f1a1b012fd32137b7ec26874d9cfefb89a1
Thomas.dryjard <newwbuiss@gmail.com>
2020-03-04 16:29:44 +0100

    navbar finit

---
+++ front/src/components/accueil/accueil.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+
+const Accueil = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Accueil
---
+++ front/src/components/navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="contentNavbar">
+            <img className="iconNavbar" src={require('./image/icon_navbar.png')}/>
+            {console.log(window.screen.width)}
+            {window.screen.width < 1280 ?
+            <div className="contentSection">
+                <Link className="navbarSection" to="/">Tous</Link>
+                <Link className="navbarSection" to="/texte-recent">Les derniers</Link>
+                <Link className="navbarSection" to="/poster-texte">Partages</Link>
+            </div>
+            :
+            <div className="contentSection">
+                <Link className="navbarSection" to="/">Tous les lyrics</Link>
+                <Link className="navbarSection" to="/texte-recent">Les derniers lyrics</Link>
+                <Link className="navbarSection" to="/poster-texte">Partages t'es lyrics</Link>
+            </div>}
+        </div>
+    )
+}
+
+export default Navbar
