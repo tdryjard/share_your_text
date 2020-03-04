7c6a7485584c113d4937e466f72aa4364b3bc9de
Thomas.dryjard <newwbuiss@gmail.com>
2020-03-04 10:12:50 +0100

    router initialisé

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/"/>
+    </Switch>
+  );
+}
+
+export default App;
